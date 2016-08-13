3cc7b2bc975baad515bb19bda49d9e5031e774af
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2016-08-13 13:50:01 +0200

    Plugin for better reloading of go server

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+function WebpackTouch(options) {
+  var options = options || {}
+  this.filename = options.filename;
+  this.delay = options.delay? parseInt(options.delay) : 0;
+  this.ignoreOnError = options.ignoreOnError;
+  if (!this.filename) {
+    throw new Error('Require filename option')
+  }
+}
+
+WebpackTouch.prototype.write = function() {
+  fs.writeFile(this.filename, 'DONE', {flag: 'w+'});
+}
+
+WebpackTouch.prototype.apply = function(compiler) {
+  compiler.plugin("done", function(stats) {
+    if (this.ignoreOnError) {
+      if (stats.hasErrors()) {
+        console.log("Not touch on error");
+        return;
+      }
+    }
+    if (this.delay) {
+      setTimeout(function() {
+        this.write();
+      }.bind(this), this.delay)
+      return
+    }
+    this.write();
+  }.bind(this));
+};
+
+module.exports = WebpackTouch;
